032f9f9a25023e8e73ebb58fd45e3ce94a1014b5
JP Dionisio <jpthedio@gmail.com>
2026-08-28 09:42:31 +0800

    no-mistakes(review): Update reviews release markers to v1.59.421

--- v3/reviews.js
+++ v3/reviews.js
@@ -1,7 +1,7 @@
 /**
  * V3 reviews page integration.
  *
- * @release v1.59.398
+ * @release v1.59.421
  *
  * Designer owns the public Reviews section. This module only:
  *   - derives the public-profile slug from /hire/{slug} and configures the
@@ -385,7 +385,7 @@
   }
 
   var api = {
-    release: 'v1.59.398',
+    release: 'v1.59.421',
     profileSlug: profileSlug,
     configureProfileRoot: configureProfileRoot,
     paintProfile: paintProfile,
